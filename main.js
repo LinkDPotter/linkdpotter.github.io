327bb9ecddcf523c740ded5819a4e6d501d3ed2a
MonkeyDLink-Potter <monkeyd.linkpotter@gmail.com>
2020-01-25 16:50:08 +0100

    update commands

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const homeImg = ["http://www.briantina.it/webdisk/articles/img-art-2689-TAP03856A.jpg", "https://jooinn.com/images/welcome-12.png", "https://www.maropost.com/wp-content/uploads/2019/06/The-Welcome-Email_06042019-01.jpg", "http://respondr.io/wp-content/uploads/2016/07/Why_Welcome_Emails_Work.jpg"];
-const img = document.querySelector('.slide');
+const homeImg = ["https://jooinn.com/images/welcome-12.png", "https://www.maropost.com/wp-content/uploads/2019/06/The-Welcome-Email_06042019-01.jpg", "http://respondr.io/wp-content/uploads/2016/07/Why_Welcome_Emails_Work.jpg"];
 var slide = 0;
 var onClick = false;
 
 function changeSlide(n) {
+    const img = document.querySelector('.slide').querySelector('img');
     onClick = true;
     slide += n;
     if (slide < 0) slide = homeImg.length - 1;
